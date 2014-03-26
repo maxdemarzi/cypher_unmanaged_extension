5089301b08e8a84969822153d6d643f43409c859
Kamran <kamranjon@gmail.com>
2014-03-26 15:01:22 -0700

    cleaning up java syntax issues

--- unmanaged-extension/src/main/java/com/cypher/neo4jextension/MyService.java
+++ unmanaged-extension/src/main/java/com/cypher/neo4jextension/MyService.java
@@ -1,14 +1,16 @@
 package com.cypher.neo4jextension;
 
 import org.codehaus.jackson.map.ObjectMapper;
+import org.json.JSONObject;
 import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.cypher.javacompat.ExecutionResult;
 import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.index.IndexManager;
+import org.neo4j.graphdb.index.ReadableIndex;
 import org.neo4j.graphdb.index.IndexHits;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
 import org.neo4j.tooling.GlobalGraphOperations;
-import scala.util.parsing.json.JSONObject;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -20,6 +22,8 @@ import java.lang.reflect.Array;
 import java.util.*;
 import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import com.algolia.search.saas.*;
 
 @Path("/import_helper")
 public class MyService {
@@ -46,44 +50,57 @@ public class MyService {
     @Path("/seed_algolia_orgs")
     public String seedAlgoliaOrgs(@Context GraphDatabaseService db) {
 
+        ReadableIndex<Node> nodeAutoIndex = db.index().getNodeAutoIndexer().getAutoIndex();
         Index index = client.initIndex("organization_development");
         List<JSONObject> algolia_objects = new ArrayList<JSONObject>();
-        for ( Node org : node_auto_index.get("type", "Organization")){
-            String[] market_names = {};
-            String market_names_flat, location = "";
+        for ( Node org : nodeAutoIndex.get("type", "Organization")){
+            ArrayList<String> market_names = new ArrayList<String>();
+            String market_names_flat = null, location = null;
             Integer n_relationships;
-            Relationship[] markets;
-            markets  = org.getRelationships("organization_in_market");
-            location = org.getRelationships("has_headquarters").getEndNode.full_name;
-            for ( Relationship market : markets ) {
-                market_names.add( market.getEndNode.name );
+            Iterable<Relationship> markets  = org.getRelationships("organization_in_market");
+            if (markets != null && markets.iterator().hasNext()) {
+                market_names_flat = "";
+                while (markets.iterator().hasNext()) {
+                    market_names_flat += markets.iterator().next().getEndNode().getProperty('name');
+                    if (markets.iterator().hasNext()) market_names_flat += ",";
+                }
             }
-
-            for ( String str : market_names) market_names_flat = market_names_flat + ", " + str;
+            Iterable<Relationship> headquarters = org.getRelationships("has_headquarters");
+            if (headquarters != null && headquarters.iterator().hasNext())
+                location = (String) headquarters.iterator().next().getEndNode().getProperty("full_name");
 
             n_relationships = Array.getLength(n.getRelationships);
 
-
-            JSONObject[] algolia_obj;
-            algolia_obj.put( "objectId", org.uuid );
-            algolia_obj.put( "type", org.type );
-            algolia_obj.put( "name", org.name );
-            algolia_obj.put( "logo_url", org.image_id );
-            algolia_obj.put( "markets", market_names_flat );
-            algolia_obj.put( "description", org.description);
-            algolia_obj.put( "primary_role", org.primary_role);
-            algolia_obj.put( "location", location);
-            algolia_obj.put( "homepage", org.homepage_url);
-            algolia_obj.put( "n_relationships", n_relationships );
-
+            JSONObject algolia_obj = new JSONObject();
+            try {
+                algolia_obj.put("objectId", org.getProperty("uuid"));
+                algolia_obj.put("type", org.getProperty("type"));
+                algolia_obj.put("name", org.getProperty("name"));
+                algolia_obj.put("logo_url", org.getProperty("image_id"));
+                algolia_obj.put("markets", market_names_flat);
+                algolia_obj.put("description", org.getProperty("description"));
+                algolia_obj.put("primary_role", org.getProperty("primary_role"));
+                algolia_obj.put("location", location);
+                algolia_obj.put("homepage", org.getProperty("homepage_url"));
+                algolia_obj.put("n_relationships", n_relationships);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             algolia_objects.add(algolia_obj);
-
             if (algolia_objects.size() == 1000) {
-                index.saveObjects(algolia_objects);
+                try {
+                    index.saveObjects(algolia_objects);
+                } catch (AlgoliaException e){
+                    e.printStackTrace();
+                }
                 algolia_objects.clear();
             }
             if (!algolia_objects.isEmpty()){
-                index.saveObjects(algolia_objects);
+                try {
+                    index.saveObjects(algolia_objects);
+                } catch (AlgoliaException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
